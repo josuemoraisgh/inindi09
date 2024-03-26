80eca2741e0ffc9604e096709bbf9821867cd042
josuemoraisgh <josuemorais@gmail.com>
2024-03-26 09:31:41 -0300

    refactor seno

--- src/main.ino
+++ src/main.ino
@@ -6,28 +6,38 @@
 #define PINPOS 6
 #define PINNEG 5
 // Valores de Funcionamento
+#define VMAX 5.0
+#define NPONTOS (20 - 1) // QUANTIDADE DE PONTOS - 1
+const int PONTOS[] = {0, 2 * NPONTOS, 2 * NPONTOS, 4 * NPONTOS};
 #define Freq 15.0
 #define T (1.0 / Freq)
-#define DELAY_US round((T / 1020.0) * 1000000.0)
+#define DELAY_US ((unsigned int)((T / PONTOS[3]) * 1000000.0))
 
 int count = 0;
+float AMPLITUDE = 2.0;            // Amplitude é de 0 a Vmax
 unsigned long previousMicros = 0; // will store last time LED was updated
-
 // Vetor de 256 posições contendo às amplitudes do PWM variando de forma senoidal
-const int seno[] = {
-    0, 5, 11, 16, 20, 24, 27, 30, 32, 32, 33, 32, 31, 29, 27, 25, 22, 19, 17, 15, 14, 13, 13, 13, 15,
-    17, 20, 23, 28, 32, 38, 43, 48, 54, 59, 63, 68, 71, 74, 76, 77, 78, 77, 76, 75, 73, 70, 68, 65, 63,
-    60, 59, 57, 57, 57, 58, 60, 62, 66, 70, 74, 79, 84, 90, 95, 100, 105, 109, 113, 116, 118, 119, 120,
-    120, 119, 118, 116, 113, 111, 108, 105, 103, 101, 99, 98, 98, 99, 100, 102, 105, 109, 113, 117, 122,
-    128, 133, 138, 143, 147, 151, 154, 156, 158, 159, 159, 158, 157, 155, 153, 150, 147, 144, 142, 139,
-    137, 136, 135, 135, 136, 138, 140, 143, 147, 151, 156, 161, 166, 171, 176, 180, 184, 187, 190, 192,
-    193, 193, 192, 191, 189, 187, 184, 181, 178, 175, 172, 170, 168, 167, 166, 167, 168, 170, 172, 176,
-    179, 184, 188, 193, 198, 203, 207, 211, 214, 217, 219, 220, 220, 220, 218, 217, 214, 211, 208, 205,
-    202, 198, 196, 193, 192, 191, 191, 191, 192, 194, 197, 201, 205, 209, 213, 218, 222, 227, 230, 234,
-    236, 238, 240, 240, 240, 238, 237, 234, 231, 228, 224, 221, 217, 214, 211, 209, 208, 207, 207, 208,
-    209, 211, 214, 218, 221, 226, 230, 234, 238, 242, 245, 248, 250, 251, 252, 251, 250, 248, 246, 243,
-    239, 236, 232, 228, 225, 221, 219, 217, 215, 214, 215, 215, 217, 219, 222, 226, 230, 234, 238, 242,
-    245, 248};
+const float seno[] = {0.0,
+                      0.07845909572784494,
+                      0.15643446504023087,
+                      0.2334453638559054,
+                      0.3090169943749474,
+                      0.3826834323650898,
+                      0.45399049973954675,
+                      0.5224985647159488,
+                      0.5877852522924731,
+                      0.6494480483301837,
+                      0.7071067811865476,
+                      0.7604059656000309,
+                      0.8090169943749475,
+                      0.8526401643540922,
+                      0.8910065241883678,
+                      0.9238795325112867,
+                      0.9510565162951535,
+                      0.9723699203976766,
+                      0.9876883405951378,
+                      0.996917333733128,
+                      1.0};
 void ledFunc() // Faz a leitura do sinal Analogico
 {
   digitalWrite(pinLED1, !digitalRead(pinLED1));
@@ -35,22 +45,23 @@ void ledFunc() // Faz a leitura do sinal Analogico
 
 void AnalogWriteFunc()
 {
-  switch (round(count / 255))
+  const int8_t aux = round(count / NPONTOS);
+  switch (round(count / NPONTOS))
   {
   case 0:
-    analogWrite(PINPOS, seno[count]);
+    analogWrite(PINPOS, (int)(255 * (AMPLITUDE / VMAX) * seno[count]));
     break;
   case 1:
-    analogWrite(PINPOS, seno[510 - count]);
+    analogWrite(PINPOS, (int)(255 * (AMPLITUDE / VMAX) * seno[PONTOS[aux] - count]));
     break;
   case 2:
-    analogWrite(PINNEG, seno[count - 510]);
+    analogWrite(PINNEG, (int)(255 * (AMPLITUDE / VMAX) * seno[count - PONTOS[aux]]));
     break;
   case 3:
-    analogWrite(PINNEG, seno[1020 - count]);
+    analogWrite(PINNEG, (int)(255 * (AMPLITUDE / VMAX) * seno[PONTOS[aux] - count]));
     break;
   }
-  if (count >= 1020)
+  if (count >= PONTOS[3])
     count = 1;
   else
     count++;
