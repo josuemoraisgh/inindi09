518218d5d933478f065bab3340d321830f7f6074
josuemoraisgh <josuemorais@gmail.com>
2024-03-28 08:31:29 -0300

    refactor sinFunc

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,25 @@
-# 1 "C:\\Users\\ADMINI~1\\AppData\\Local\\Temp\\tmprg3gzivp"
-#include <Arduino.h>
-# 1 "C:/SourceCode/inindi09/src/main.ino"
 #include <Arduino.h>
 #include <math.h>
 #include "digitalFilter.h"
-#define NUMTHREADS 2
+#define NUMTHREADS 2 // Deve-se sempre declarar o NUMTHREADS antes de "inindThread.h"
 #include "inindThread.h"
 
-
-#define pinLED1 13
+// Pinos para o PWM
+#define pinLED1 13 // Configura o pino de Saida do LED
 #define PINPOS 6
 #define PINNEG 5
-
+// Valores de Funcionamento
 #define VMAX 5.0
-#define NPONTOS (20 - 1)
+#define NPONTOS (20 - 1) // QUANTIDADE DE PONTOS - 1
 const int PONTOS[] = {0, 2 * NPONTOS, 2 * NPONTOS, 4 * NPONTOS};
 #define Freq 15.0
 #define T (1.0 / Freq)
-#define SIN_INTERVAL ((unsigned int)((1000000.0 / (Freq*PONTOS[3]))))
+#define SIN_INTERVAL ((unsigned int)((1000000.0 / (Freq * PONTOS[3]))))
 
 int count = 0;
-float AMPLITUDE = 2.0;
-unsigned long previousMicros = 0;
-
+float AMPLITUDE = 2.0;            // Amplitude é de 0 a Vmax
+unsigned long previousMicros = 0; // will store last time LED was updated
+// Vetor de 256 posições contendo às amplitudes do PWM variando de forma senoidal
 const float seno[] = {0.0,
                       0.07845909572784494,
                       0.15643446504023087,
@@ -44,16 +41,11 @@ const float seno[] = {0.0,
                       0.9876883405951378,
                       0.996917333733128,
                       1.0};
-void ledFunc();
-void sinFunc();
-void setup();
-void loop();
-#line 44 "C:/SourceCode/inindi09/src/main.ino"
-void ledFunc()
+void ledFunc() // Faz a leitura do sinal Analogico
 {
   digitalWrite(pinLED1, !digitalRead(pinLED1));
 }
-
+/*
 void sinFunc()
 {
   const int8_t aux = round(count / NPONTOS);
@@ -77,19 +69,27 @@ void sinFunc()
   else
     count++;
 }
+*/
+void sinFunc()
+{
+  const int8_t aux = count % NPONTOS;
+  analogWrite(aux <= 1 ? PINPOS : PINNEG, (int)(255 * (AMPLITUDE / VMAX) * seno[abs(PONTOS[aux] - count)]));
+  count = (count >= PONTOS[3]) ? 1 :count+1;
+}
 
 void setup()
 {
   Serial.begin(19200);
   pinMode(PINPOS, OUTPUT);
   pinMode(PINNEG, OUTPUT);
-  pinMode(pinLED1, OUTPUT);
+  pinMode(pinLED1, OUTPUT); // Habilita interrupções globais
   analogWrite(PINNEG, 0);
   analogWrite(PINPOS, 0);
-  threadSetup(ledFunc,2000);
+  threadSetup(ledFunc, 2000); // parametros:funcão,intervalo,funcão,intervalo,...
 }
 
-void loop() {
+void loop()
+{
   unsigned long currentMicros = micros();
   if (((currentMicros - previousMicros >= 0) && (currentMicros - previousMicros >= SIN_INTERVAL)) ||
       ((currentMicros - previousMicros < 0) && (currentMicros - 0 + 16383 - previousMicros >= SIN_INTERVAL)))
